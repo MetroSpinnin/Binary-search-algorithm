472ecd49f50f349e281f97c6f0de260c721bd72b
Jim <46245794+MetroSpinnin@users.noreply.github.com>
2019-03-26 19:08:09 +0100

    Update binarySearch.php

--- binarySearch.php
+++ binarySearch.php
@@ -1,8 +1,20 @@
 <?php
 
 	function RealBinarySearch($arr, $data)
-	{
+	{	
+		/**
+		* The $left has to be attributed to zero
+		* because it is assumed to be the Lil
+		*/
+		
 		$left = 0;
+		
+		/**
+		* The right side of the array carries
+		* the total number of elements in the
+		* array
+		*/
+		
 		$right = count($arr)-1;
 		while($left <= $right)
 		{
@@ -17,11 +29,23 @@
 			$right = $middleValue - 1;
 		}
 		else 
-		{
+		{	
+			/**
+			* If the values does not exist
+			* after the while loop then kindly
+			* return the middlevalue
+			*/
+			
 			return $middleValue;
 		}
 		return NULL;
 	}  
+
+/**
+* Create an object and pass the 
+* values to it and scrutinize using
+* the BinarySearch function created
+*/
 
 $arrayOfValues = ['2','3','6','1'];
 $dataToSearchFor = 1;
